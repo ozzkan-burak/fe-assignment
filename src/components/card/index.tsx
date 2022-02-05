49a884d7b73331da49f6a40293908a4c0f798889
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-05 23:55:15 +0300

    popular products ready

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -12,38 +12,53 @@ const Card = (data: any) => {
 
   const { id, label, price, discountLabel, firstPrice, details } = data.item;
 
+  console.log({ discountLabel });
+  console.log({ firstPrice });
+
   return (
-    <div className='card-container'>
-      <div className='card-header'>
-      </div>
-      <div className='card-body'>
-        <img src={bitmap} alt={label} />
-      </div>
-      <div className='card-details'>
-        {
-          details !== null && details !== undefined ? details.map((item: IDetail, i: any) => (
-            <Details detail={item} />
-          ))
-            : ''
-        }
-      </div>
-      <div className="card-footer">
-        <div className="card-price">
-          <span className="card-footer__price-wrapper">
-            <span className="price-wrapper-first">{firstPrice}</span>
-            <span className="price-wrapper-price">{price}</span>
-          </span>
-          <span className="discount">
-            {discountLabel}
-          </span>
+    <div className="card">
+      <div className='card-container'>
+        <div className='card-header'>
+          {label}
         </div>
-        <div className="card-button">
-          <button className="card-button__button">
-            <img src={add} alt="add" />
-          </button>
+        <div className='card-body'>
+          <img src={bitmap} alt={label} />
+        </div>
+        <div className='card-details'>
+          {
+            details !== null && details !== undefined ? details.map((item: IDetail, i: any) => (
+              <Details key={i} detail={item} />
+            ))
+              : ''
+          }
+        </div>
+        <div className="card-footer">
+          <div className="card-price">
+            <span className="card-footer__price-wrapper">
+              {
+                discountLabel !== null && discountLabel !== undefined ?
+                  <span className="price-wrapper-first">{firstPrice} TL</span>
+                  : ''
+              }
+              <span className="price-wrapper-price">{price} TL</span>
+            </span>
+            {
+              discountLabel !== null && discountLabel !== undefined ?
+                <span className="discount">
+                  {discountLabel}
+                </span>
+                : ''
+            }
+          </div>
+          <div className="card-button">
+            <button className="card-button__button">
+              <img src={add} alt="add" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
